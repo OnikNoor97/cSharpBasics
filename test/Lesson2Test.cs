61adde2f723f7b357415fc21c6fb4e2539cd7f8f
Onik Noor <O_Noor@live.co.uk>
2020-02-17 13:47:36 +0000

    Added fact attribute, finished test 3 and test 4

--- test/Lesson2Test.cs
+++ test/Lesson2Test.cs
@@ -1,4 +1,6 @@
 using System;
+using System.Collections.Generic;
+using System.Linq;
 using src;
 using Xunit;
 
@@ -8,40 +10,54 @@ namespace test
     {
         Lesson2 l2 = new Lesson2();
 
+        [Fact]
         public void q1Test() => Assert.Equal(13, l2.q1(5, 6, 2));
 
+        [Fact]
         public void q2Test() => Assert.Equal(5, l2.q2(2, 4, 6, 8));
 
-        public void q3Test()
-        {
-            Assert.Equal(13, l2.q1(5, 6, 2));
-        }
+        [Fact]
+        public void q3Test() => Assert.Equal("Wow! You will be 24 soon!", l2.q3(23));
 
+        [Fact]
         public void q4Test()
         {
-            Assert.Equal(13, l2.q1(5, 6, 2));
+            List<String> expected = new List<String>();
+            expected.Add("30 30 30 30");
+            expected.Add("30303030");
+            
+            List<String> actual = l2.q4(30);
+            bool checker = expected.SequenceEqual(actual);
+
+            Assert.True(checker, "Seems like the list is not equal, check if they are in order");
+
         }
 
+        [Fact]
         public void q5Test()
         {
             Assert.Equal(13, l2.q1(5, 6, 2));
         }
 
+        [Fact]
         public void q6Test()
         {
             Assert.Equal(13, l2.q1(5, 6, 2));
         }
 
+        [Fact]        
         public void q7Test()
         {
             Assert.Equal(13, l2.q1(5, 6, 2));
         }
 
+        [Fact]
         public void q8Test()
         {
             Assert.Equal(13, l2.q1(5, 6, 2));
         }
-
+        
+        [Fact]
         public void q9Test()
         {
             Assert.Equal(13, l2.q1(5, 6, 2));
