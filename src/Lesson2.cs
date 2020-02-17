4ef48fa0b736464587e818c646f603d74ef3317e
Onik Noor <O_Noor@live.co.uk>
2020-02-17 13:40:13 +0000

    Removed using ArrayList

--- src/Lesson2.cs
+++ src/Lesson2.cs
@@ -1,5 +1,5 @@
 using System;
-using System.Collections;
+using System.Collections.Generic;
 
 namespace src
 {
@@ -26,41 +26,41 @@ namespace src
         }
 
         // Write a C# program to that takes a number as input and display it four times in a row (separated by blank spaces), and then four times in the next row, with no separation.
-        // For example: I input 25, it should return 25 25 25 25 and 25252525
-        public ArrayList q4()
+        // For example: I input 25, it should return 25 25 25 25 and then 25252525
+        public List<String> q4(int x)
         {
             throw new NotImplementedException();
         }
 
         // Write a C# program to convert from celsius degrees to Kelvin AND Fahrenheit
-        public ArrayList q5(int x)
+        public List<String> q5(int x)
         {
             throw new NotImplementedException();
         }
 
         // Write a C# program where all numbers are returned from a range from x to y
         // Hint: This will require a for loop within range
-        public ArrayList q6(int x, int y)
+        public List<String> q6(int x, int y)
         {
             throw new NotImplementedException();
         }
 
         // Write a C# program where all odd numbers are returned from a range from x to y
         // Hint: This will require a for loop within range
-        public ArrayList q7(int x, int y)
+        public List<String> q7(int x, int y)
         {
             throw new NotImplementedException();
         }
 
         // Write a C# program where all even numbers are returned from a range from x to y
         // Hint: This will require a for loop within range
-        public ArrayList q8()
+        public List<String> q8()
         {
             throw new NotImplementedException();
         }
 
         // Write a C# program where all prime numbers are returned from a range from x to y
-        public ArrayList q9() // Bonus - This will push you but answer will be given by me 
+        public List<String> q9() // Bonus - This will push you but answer will be given by me 
         {
             throw new NotImplementedException();
         }
