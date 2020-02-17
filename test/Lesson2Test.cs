614dc981a1177fd4e8a23b019cbfaea2329e426d
Onik Noor <O_Noor@live.co.uk>
2020-02-17 14:07:25 +0000

    Lesson 2, Finished All Tests Questions

--- test/Lesson2Test.cs
+++ test/Lesson2Test.cs
@@ -70,19 +70,47 @@ namespace test
         [Fact]        
         public void q7Test()
         {
-            Assert.Equal(13, l2.q1(5, 6, 2));
+            List<int> expected = new List<int>();
+            expected.Add(7);
+            expected.Add(9);
+            expected.Add(11);
+            expected.Add(13);
+            expected.Add(15);
+
+            List<int> actual = l2.q7(7, 16);
+            bool checker = expected.SequenceEqual(actual);
+
+            Assert.True(checker, "Seems like the list is not equal, are you sure you are doing in the right range/order?");
         }
 
         [Fact]
         public void q8Test()
         {
-            Assert.Equal(13, l2.q1(5, 6, 2));
+            List<int> expected = new List<int>();
+            expected.Add(8);
+            expected.Add(10);
+            expected.Add(12);
+            expected.Add(14);
+            expected.Add(16);
+
+            List<int> actual = l2.q8(7, 16);
+            bool checker = expected.SequenceEqual(actual);
+
+            Assert.True(checker, "Seems like the list is not equal, are you sure you are doing in the right range/order?");
         }
         
         [Fact]
         public void q9Test()
         {
-            Assert.Equal(13, l2.q1(5, 6, 2));
+            List<int> expected = new List<int>();
+            expected.Add(7);
+            expected.Add(11);
+            expected.Add(13);
+
+            List<int> actual = l2.q9(7, 16);
+            bool checker = expected.SequenceEqual(actual);
+
+            Assert.True(checker, "Seems like the list is not equal, are you sure you are doing in the right range/order?");
         }
     }
 }
