eab69913d106989127766e8d39989d375ef56dcd
Onik Noor <O_Noor@live.co.uk>
2020-03-08 23:30:20 +0000

    Fixed Lesson 1 test

--- test/Lesson1Test.cs
+++ test/Lesson1Test.cs
@@ -14,10 +14,10 @@ namespace test
         public void subtractTest() => Assert.Equal(4, l1.subtract(5, 1));
 
         [Fact]
-        public void multiplyTest() => Assert.Equal(2, l1.multiply(1, 1));
+        public void multiplyTest() => Assert.Equal(10, l1.multiply(5, 2));
 
         [Fact]
-        public void divideTest() => Assert.Equal(2, l1.divide(1, 1));
+        public void divideTest() => Assert.Equal(5, l1.divide(10, 2));
 
     }
 }
